2fc8c58c6d8d6a7b4acddafb0e13641278906184
Matthew Barton <mbarton@trinity.edu>
2021-10-03 15:50:11 -0500

    Read data from csv and tokenize with spaCy

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+import spacy
+import csv
+
+
+with open("Critic Reviews Data Sample.csv") as fp:
+    reader = csv.reader(fp, delimiter=",", quotechar='"')
+    # next(reader, None)  # skip the headers
+    data_read = [row for row in reader]
+
+# Data starts at row 1, content is at index 4
+sent = data_read[1][4]
+print(sent)
+
+
+nlp = spacy.load("en_core_web_trf")
+doc = nlp(sent)
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
